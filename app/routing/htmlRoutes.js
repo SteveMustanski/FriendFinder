10cc33017406fcfebd9b05347224993723e85936
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-07 17:31:19 -0500

    added skeleton survey page and routing to survey page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,7 +4,12 @@ const path = require("path");
 // routing
 
 module.exports = function(app) {
-  // GET Requests to home
+  // GET requests
+
+  // route to survey page
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
   // If no matching route is found default to home
   app.get("/", function(req, res) {
