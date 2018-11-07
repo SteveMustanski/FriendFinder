86dd8bc6d8bc94f9369adeb0bf5c4abae99f2940
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-07 17:12:12 -0500

    first commit with simple express server and readme updates

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# FriendFinder
-A boot camp project using node
+## FriendFinder
+This project is part of a coding bootcamp the puts introduces node and express to serve an application to the client
+
+The application asks the user several questions and matches that data with existing users to suggest a friend.
+
+The application does not have an real code to suggest a match, but simple math just for the purposes of having the application work.
+
+# Depenancies
+* express
+* path
+
+
+
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// set up the routes
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+
+// start the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
